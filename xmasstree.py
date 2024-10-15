211be58049d7ae646f52bb27e6ce52fc0381ea67
Isaul <tiradolisaul@gmail.com>
2024-10-14 20:09:16 -0700

    stage 1 completed: tree leaves printed

--- xmasstree.py
+++ xmasstree.py
@@ -1,7 +1,26 @@
 # X-Mas Tree Project
 
+def make_tree(height: int) -> None:
+    space = ' '
+    star = '*'
+    for i in range(height):
+        stars = star * (2 * i + 1)
+        spaces = space * (height - i - 1)
+        print(spaces + stars)
+
+
+def read_height() -> int:
+    try:
+        height = int(input())
+        return height
+    except ValueError:
+        print("Error: Enter a number.")
+        return -1
+
+
 def main():
-    print("MAIN")
+    height = read_height()
+    make_tree(height)
 
 if __name__ == "__main__":
     main()
